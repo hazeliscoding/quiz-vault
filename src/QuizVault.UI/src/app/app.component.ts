d6e8e686c92684e1bdfca0ab8597a8e32f5b75dc
hazeliscoding <hazel.granados@protonmail.com>
2024-10-02 10:05:39 -0500

    fix: 🐛 remove footer

--- src/QuizVault.UI/src/app/app.component.ts
+++ src/QuizVault.UI/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./core/components/header/header.component";
-import { FooterComponent } from "./core/components/footer/footer.component";
+import { HeaderComponent } from './core/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'QuizVault.UI';
